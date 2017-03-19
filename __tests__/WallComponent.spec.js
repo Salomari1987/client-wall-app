58f83975cf36968409dd3a57d4467bd3d556a096
Salah Alomari <s.z.alomari.1987@gmail.com>
2017-03-20 01:15:12 +0200

    Test WallComponent

---
+++ __tests__/WallComponent.spec.js
@@ -0,0 +1,41 @@
+import { mount, shallow } from 'enzyme';
+import React from 'react';
+jest.mock('../app/stores/WallStore.jsx');
+jest.mock('../app/actions/WallActions.jsx');
+import {expect} from 'chai';
+
+import Wall from '../app/components/Wall.jsx'
+import Message from '../app/components/Message.jsx'
+import MessageInput from '../app/components/MessageInput.jsx'
+import WallStore from '../app/stores/WallStore.jsx'
+import WallActions from '../app/actions/WallActions.jsx'
+import ReactDOM from 'react-dom';
+
+describe('<Wall />', () => {
+	var wall;
+	beforeEach(function() {
+		WallStore.getState.mockReturnValue({
+		  'token': '',
+		  'messages': [{'author': 'salomari', 'body': 'hi'}]
+		});
+		wall = mount(<Wall/>);
+		Wall.prototype.scrollToBottom = jest.fn()
+		spyOn(Wall.prototype, 'sendMessage')
+	})
+  
+  it('does not contain a <MessageInput/> component when user is not logged in', function () {
+
+    expect(wall.find(MessageInput)).to.have.length(0);
+  });
+  
+  it('contains an <MessageInput/> component when user is logged in', function () {
+		
+		wall.setState({token: 'username'})    
+    expect(wall.find(MessageInput)).to.have.length(1);
+  });
+
+  it('contains an <Message/> component when user is not logged in', function () {
+		
+    expect(wall.find(Message)).to.have.length(1);
+  });
+})
