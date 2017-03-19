1d0aeb022599f2f476b09dabf008e8d936d86f4c
Salah Alomari <s.z.alomari.1987@gmail.com>
2017-03-20 01:14:59 +0200

    Test MessageInput component

---
+++ __tests__/MessageInput.spec.js
@@ -0,0 +1,28 @@
+import { mount, shallow } from 'enzyme';
+import React from 'react';
+import chai from 'chai'
+import {expect} from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai'
+import MessageInput from '../app/components/MessageInput.jsx'
+chai.use(sinonChai);
+
+describe('<MessageInput />', () => {
+  
+  it('does not contain a <MessageInput/> component when user is not logged in', function () {
+	var sendMessage = sinon.spy();
+	const input = mount(<MessageInput sendMessage={sendMessage}/>);
+	input.find('button').simulate('submit')
+    expect(sendMessage).to.have.been.called;
+  });
+
+  it('does not contain a <MessageInput/> component when user is not logged in', function () {
+	var updateMessage = sinon.spy()
+	var sendMessage = sinon.spy();
+	const input = mount(<MessageInput updateMessage={updateMessage} sendMessage={sendMessage}/>);
+	var event = {target: {value: 'My new value'}}
+	input.find('input').simulate('change', event)
+    
+    expect(updateMessage).to.have.been.called;
+  });
+})
